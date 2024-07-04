51d7bd99eb78ae4978f1cc7d22d018d365822c99
Turner Naef <Turner.Naef@gmail.com>
2024-07-03 21:40:40 -0600

    Initial Rust API

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+mod test;
+
+#[macro_use]
+extern crate rocket;
+
+#[get("/world")]
+fn world() -> &'static str {
+    "Hello, world!"
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/hello", routes![world])
+}
